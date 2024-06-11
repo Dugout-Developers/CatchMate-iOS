3a4925b3e7c29b3d08bafbe6154baeee723de10c
Tori <ttang1135@naver.com>
2024-06-11 23:49:10 +0900

    init project

---
+++ CatchMate/CatchMateUITests/CatchMateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CatchMateUITestsLaunchTests.swift
+//  CatchMateUITests
+//
+//  Created by 방유빈 on 6/11/24.
+//
+
+import XCTest
+
+final class CatchMateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
