a879f1e23a7a02dfc2e0862c8f6f5aa7370b9c1b
Tori <ttang1135@naver.com>
2024-06-12 15:01:56 +0900

    feat: INFO 설정 및 프로젝트 세팅

---
+++ CatchMate/CatchMate/Utilities/Extensions/Bundle+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  Bundle+Extension.swift
+//  CatchMate
+//
+//  Created by 방유빈 on 6/12/24.
+//
+
+import Foundation
+
+extension Bundle {
+    var kakaoLoginKey: String? {
+           guard let file = self.path(forResource: "APIKeys", ofType: "plist"),
+                 let resource = NSDictionary(contentsOfFile: file),
+                 let key = resource["KAKAO_LOGIN_API"] as? String else {
+               print("Kakao API Key 얻기 실패")
+               return nil
+           }
+           return key
+       }
+    
+    var naverLoginClientID: String? {
+           guard let file = self.path(forResource: "APIKeys", ofType: "plist"),
+                 let resource = NSDictionary(contentsOfFile: file),
+                 let key = resource["NAVER_LOGIN_API_ClientID"] as? String else {
+               print("Kakao API Key 얻기 실패")
+               return nil
+           }
+           return key
+       }
+    
+    var naverLoginClientSecret: String? {
+           guard let file = self.path(forResource: "APIKeys", ofType: "plist"),
+                 let resource = NSDictionary(contentsOfFile: file),
+                 let key = resource["NAVER_LOGIN_API_ClientSecret"] as? String else {
+               print("Kakao API Key 얻기 실패")
+               return nil
+           }
+           return key
+       }
+}
