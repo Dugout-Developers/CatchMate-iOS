31e25b13ae0808646abd4cee96f66c3c4fce4856
Tori <ttang1135@naver.com>
2024-06-12 15:11:19 +0900

    feat: 클린 아키텍쳐 구조 기본 폴더링

---
+++ CatchMate/CatchMate/DesignSystem/Components/TeamButton.swift
@@ -0,0 +1,8 @@
+//
+//  TeamButton.swift
+//  CatchMate
+//
+//  Created by 방유빈 on 6/12/24.
+//
+
+import Foundation
---
+++ CatchMate/CatchMate/Domain/UseCases/LoginUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  LoginUseCase.swift
+//  CatchMate
+//
+//  Created by 방유빈 on 6/12/24.
+//
+
+import Foundation
