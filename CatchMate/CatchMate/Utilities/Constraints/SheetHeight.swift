9e8fa5974b0f491373fa9fe668684c84dd3fa5b0
Tori <ttang1135@naver.com>
2024-06-22 23:56:42 +0900

    feat: 등록 뷰 와이어프레임 - 선호 성별까지

---
+++ CatchMate/CatchMate/Utilities/Constraints/SheetHeight.swift
@@ -0,0 +1,14 @@
+//
+//  SheetHeight.swift
+//  CatchMate
+//
+//  Created by 방유빈 on 6/19/24.
+//
+
+import UIKit
+
+enum SheetHeight {
+    static let large = Screen.height * 3/4
+    static let medium = Screen.height / 2.0 + 50.0
+    static let low = Screen.height / 3.0 + 10.0
+}
