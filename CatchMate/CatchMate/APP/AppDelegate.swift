c56c12c9335e0f820b9403ae29d17b3396aec7df
방유빈 <58802345+bangtori@users.noreply.github.com>
2024-07-09 00:32:39 +0900

    fastlane, githubaction 설정 (#31)
    * feat: fastlane 설정
    
    * feat: githubAction 배포 자동화 코드 적용
    
    * feat: githubAction 배포 자동화 코드 적용

--- CatchMate/CatchMate/APP/AppDelegate.swift
+++ CatchMate/CatchMate/APP/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         DispatchQueue.global(qos: .background).async {
             if let kakaoAppKey = Bundle.main.kakaoLoginAPPKey {
+                print("Kakao Login API Key: \(kakaoAppKey)")
                 RxKakaoSDK.initSDK(appKey: kakaoAppKey)
             }
             
