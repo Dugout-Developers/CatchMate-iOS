cdac26c7d17ac5b2c357d96b8794e74546eecb8c
Tori <ttang1135@naver.com>
2024-07-30 13:18:53 +0900

    fix: Hi-Fi 디자인 적용 - 채팅방 List View

--- CatchMate/CatchMate/Utilities/Extensions/Date+Extension.swift
+++ CatchMate/CatchMate/Utilities/Extensions/Date+Extension.swift
@@ -15,4 +15,33 @@ extension Date {
     func startOfDay() -> Date {
         return Calendar.current.startOfDay(for: self)
     }
+    
+    func isSameDay(as date: Date) -> Bool {
+        let calendar = Calendar.current
+        return calendar.isDate(self, inSameDayAs: date)
+    }
+    
+    func timeAgoDisplay() -> String {
+        let calendar = Calendar.current
+        let now = Date()
+        let components = calendar.dateComponents([.minute, .hour, .day], from: self, to: now)
+        
+        if let minute = components.minute, minute < 1 {
+            return "방금"
+        }
+        
+        if let minute = components.minute, minute < 60 {
+            return "\(minute)분 전"
+        }
+        
+        if let hour = components.hour, hour < 24 {
+            return "\(hour)시간 전"
+        }
+        
+        if let day = components.day {
+            return "\(day)일 전"
+        }
+        
+        return "오래 전"
+    }
 }
