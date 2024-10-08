5044c733f766c141f00a9d0295a918766ebfb458
Tori <ttang1135@naver.com>
2024-10-08 18:52:36 +0900

    feat: 알림 데이터 DTO 임시

---
+++ CatchMate/CatchMate/Data/DTO/NotificationListResponse.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationListResponse.swift
+//  CatchMate
+//
+//  Created by 방유빈 on 10/8/24.
+//
+
+import Foundation
+
+struct Notification: Codable {
+    let notificationId: Int
+    let boardId: Int
+    let title: String
+    let body: String
+    let createdAt: String
+    let read: Bool
+    
+
+}
+
+struct NotificationListResponse: Codable {
+    let content: [Notification]
+}
